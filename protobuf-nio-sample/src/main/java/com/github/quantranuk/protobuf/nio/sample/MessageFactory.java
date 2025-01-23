09f779309b8fcc3490ac2d78137fc8a763d1955e
J08nY <johny@neuromancer.sk>
2025-01-23 10:35:10 +0100

    Move back to original package.

---
+++ protobuf-nio-sample/src/main/java/com/github/quantranuk/protobuf/nio/sample/MessageFactory.java
@@ -0,0 +1,30 @@
+package com.github.quantranuk.protobuf.nio.sample;
+
+public final class MessageFactory {
+
+    private final HeartBeat.HeartBeatRequest.Builder heartbeatRequestBuilder;
+    private final HeartBeat.HeartBeatResponse.Builder heartbeatResponseBuilder;
+    private long requestCount = 0;
+    private long responseCount = 0;
+
+    public MessageFactory() {
+        heartbeatRequestBuilder = HeartBeat.HeartBeatRequest.newBuilder();
+        heartbeatResponseBuilder = HeartBeat.HeartBeatResponse.newBuilder();
+    }
+
+    public HeartBeat.HeartBeatRequest createHeartBeatRequest() {
+        return heartbeatRequestBuilder
+                .setRequestTimeMillis(System.currentTimeMillis())
+                .setRequestMessage("Ping-" + requestCount++)
+                .build();
+    }
+
+    public HeartBeat.HeartBeatResponse createHeartBeatResponse(long heartBeatRequestRequestTimeMillis) {
+        return heartbeatResponseBuilder
+                .setRequestTimeMillis(heartBeatRequestRequestTimeMillis)
+                .setResponseTimeMillis(System.currentTimeMillis())
+                .setResponseMessage("Pong-" + responseCount++)
+                .build();
+    }
+
+}
